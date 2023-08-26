41e25d45ea5d5e035bf07bab4db11a28bc91bb13
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:18:52 +0530

    Update and rename Q-7 to C7.cpp

--- C7.cpp
+++ C7.cpp
@@ -1,3 +1,8 @@
+/* Write a program in C++ to use map associative container. The keys will be the names of states and the values will be the populations of the states.
+When the program runs, the user is prompted to type the name of a state. The program then looks in the map, using the state name as an index and
+returns the population of the state.*/
+
+
 #include <iostream>
 #include <map>
 using namespace std;
