940010323025ef84d0911f3c251c65202c3abe05
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:12:20 +0530

    Update and rename Q-3 to A3.cpp

--- A3.cpp
+++ A3.cpp
@@ -1,3 +1,12 @@
+/*Imagine a publishing company which does marketing for book and audio cassette versions. Create a class publication that stores the title (a string) and price (type float) of publications. From this class derive two classes: book which adds a page count (type int) and tape which adds a playing time in minutes (type float).
+
+Write a program that instantiates the book and tape class, allows user to enter data and
+
+displays the data members. If an exception is caught, replace all the data member values with
+
+zero values.*/
+
+
 #include <iostream>
 using namespace std;
 class publication
