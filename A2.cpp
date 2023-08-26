6e5acbbcac4fa76fb7780804edc70349fc1d22eb
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:10:48 +0530

    Update and rename Q-2 to A2.cpp

--- A2.cpp
+++ A2.cpp
@@ -1,3 +1,10 @@
+/* Develop a program in C++ to create a database of student's information system containing the following information: Name,
+Roll number, Class, Division, Date of Birth, Blood group, Contact address, Telephone number, Driving license no. and other. 
+Construct the database with suitable member functions. Make use of constructor, default constructor, copy constructor, destructor, static member functions, 
+friend class, this pointer, inline code and dynamic memory allocation operators-new and delete as well as exception handling. */
+
+
+
 #include <iostream>
 using namespace std;
 class student
