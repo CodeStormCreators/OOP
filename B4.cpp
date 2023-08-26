b7c5aa8047290ccaf0db1e52f74ab48b354be7a1
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:13:53 +0530

    Update and rename Q-4 to B4.cpp

--- B4.cpp
+++ B4.cpp
@@ -1,3 +1,7 @@
+/*Write a C++ program that creates an output file, writes information to it, closes the file, open it again as an input file and 
+read the information from the file.*/
+
+
 #include <iostream>
 #include <fstream>
 using namespace std;
