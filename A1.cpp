48aca775a32735a62938ba9179cfda4d6704ceea
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:05:22 +0530

    Update and rename Q-1 to A1.cpp

--- A1.cpp
+++ A1.cpp
@@ -1,3 +1,10 @@
+/*Implement a class complex which represents the Complex Number data type, Implement the following 
+1. Constructor(Including a default constuctor which creates the complex number 0+0i)
+2. Overload operator + to add two complex numbers.
+3. Overload operator * to multiply two complex numbers.
+4. Overload operator << and >> to print and read Complex Numbers. */
+
+
 #include <iostream>
 using namespace std;
 class complex
